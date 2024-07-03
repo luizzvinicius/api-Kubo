2730bf95e6cb29808f828d703adf968894059b4e
Luiz Vinicius <viniciusadr244@gmail.com>
2024-07-03 11:22:15 -0300

    update

---
+++ info.js
@@ -0,0 +1,16 @@
+// Situação atual cluster
+const peers = {
+  cluster_peers: [
+    "12D3KooWFmEuwNuYLFgWo3Lysi3kzbpRnQiu8HAN718Lti33YcD5",
+    "12D3KooWQgGh94w7ovz53iQhuVNNJBe65smLFFUt6QWRw3exd3wE",
+    "12D3KooWSWHtC7CAWFKUuNhFUvTo4nSh6YF7hkRsCqMSuPbAty8F" // local que quero adicionar
+  ],
+};
+//QmUxDhypophpTju34uZxHop9NEGXm5tEDsxqgjKxJJghvb
+// /ip4/172.18.0.5/tcp/9096/p2p/12D3KooWQgGh94w7ovz53iQhuVNNJBe65smLFFUt6QWRw3exd3wE
+const CID = {
+  files: [
+    "QmS4wbTJkoc43n8kurfrkxGKYpbC7DvRo7TwMoYHPAK2a5", // edital proficiência
+    "QmZ95WbmjD9TqvNoGpao1bZkH3ryWKHUVwExkpEjdo3Ghi", // teste IPFS
+  ],
+};
